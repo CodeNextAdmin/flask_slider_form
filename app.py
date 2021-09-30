dc4540dfdb1bfebc11ce6e8fe92f70335c3a0195
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2021-09-30 16:17:38 -0700

    added flask files

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route("/test", methods=["POST"])
+def test():
+    slider_value = request.form["slider"]
+
+    # Send the data to the new route /test. 
+    # Try changing it so it just moves a servo to the desired angle.
+    # Make sure to validate degrees between 0 - 180.
+    return slider_value
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
